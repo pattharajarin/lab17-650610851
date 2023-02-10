c1f9da872fec6e3539511715ee6a6ebc64e4bdfb
pattharajarin <pattharajarin_p@cmu.ac.th>
2023-02-10 17:39:31 +0700

    ok

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,36 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a, int N , int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *a++;
+        if((i+1)%M ==0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a, int N , int M){
+    for(int i = 0; i < N*M; i++){
+        *(a+i) = (rand()%101) / 100.0;
+    }
+}
+void findRowSum(const double *a,double *b,int N,int M){
+    for(int i = 0; i < N; i++){
+        *(b+i) = 0;
+        for(int j = 0;j < M;j++){
+           *(b+i) += *(a +(i*M)+j);
+
+        }
+    }
+}
+void findColSum(const double *a,double *b,int N,int M){
+    for(int j = 0; j < M; j++){
+        *(b+j) = 0;   
+        for(int i = 0;i < N;i++){
+           *(b+j) += *(a +(i*M)+j);  
+
+        }
+    }
+}
